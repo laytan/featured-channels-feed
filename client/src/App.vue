ca5527eb07e3e47c88a442251161c691a7d2f879
Laytan <laytanlaats@hotmail.com>
2021-01-11 20:44:38 +0100

    setup hello world vue app with tailwind

---
+++ client/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="grid place-items-center h-screen text-3xl">Hello World</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+  setup() {},
+});
+</script>
